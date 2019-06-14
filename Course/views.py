5e88a4d68069bab6fbe1f3128aa45e0975cf265c
yijiull <1147161372@qq.com>
2019-06-15 01:31:30 +0800

    创建了数据库，完成了简单的登录功能

--- Course/views.py
+++ Course/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import User
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from .util import verify_password
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login_verify(request):
+    id = request.POST["userid"]
+    password = request.POST["password"]
+    print(id, password)
+    user = get_object_or_404(User, u_id=id)
+    if verify_password(user.u_pwdhash, password):
+        return render(request, 'index.html', {'user': user})
+    else:
+        return render(request, 'index.html', {'user': user, 'error': '用户名或密码错误'})
+
+
+def login(request):
+    return render(request, 'login.html')
--- PKI/settings.py
+++ PKI/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Course.apps.CourseConfig',
+    # 'CloudDisk.apps.ClouddiskConfig',
+    # 'Email.apps.EmailConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
