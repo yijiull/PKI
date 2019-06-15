ed0c5c9c636cc73d6350ecb16981cecd34c08e99
yijiull <1147161372@qq.com>
2019-06-15 09:44:47 +0800

    fix bug

--- Course/views.py
+++ Course/views.py
@@ -6,6 +6,7 @@ from .models import User
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .util import verify_password
 
@@ -17,12 +18,18 @@ def index(request):
 def login_verify(request):
     id = request.POST["userid"]
     password = request.POST["password"]
-    print(id, password)
-    user = get_object_or_404(User, u_id=id)
+    # print(id, password)
+
+    # user = get_object_or_404(User, u_id=id)
+    try:
+        user = User.objects.get(u_id=id)
+    except User.DoesNotExist:
+        return render(request, 'login.html', {'error': '好像不存在该学号:)'})
     if verify_password(user.u_pwdhash, password):
         return render(request, 'index.html', {'user': user})
+        # return HttpResponseRedirect(reverse('Course:index', kwargs={'user': user}))
     else:
-        return render(request, 'index.html', {'user': user, 'error': '用户名或密码错误'})
+        return render(request, 'login.html', {'error': '密码错误:)<br>请重新输入密码~'})
 
 
 def login(request):
